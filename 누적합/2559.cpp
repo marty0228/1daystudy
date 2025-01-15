e312581a901b405a3b9085b0921bdbc52b69bf33
marty0228 <dhlee0228@naver.com>
2025-01-15 22:48:21 +0900

    배열에 합계를 누적해서 넣는 문제

---
+++ 누적합/2559.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int N,K;
+
+int psum[100004];
+int ret=-10000001;
+
+
+int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+	cout.tie(nullptr);
+	
+    cin>>N>>K;
+
+    for(int i=1;i<=N;i++){
+        int input;
+        cin>>input;
+        psum[i]=psum[i-1]+input;
+    }
+
+    for(int i=K;i<=N;i++){
+        ret=max(ret,psum[i]-psum[i-K]);
+    }
+    cout<<ret;
+    
+	return 0;
+}
+
+
+
+
+
