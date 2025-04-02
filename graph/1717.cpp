216621612b1ca69ed5b514d7ba35e3785664f7fe
marty0228 <dhlee0228@naver.com>
2025-04-02 22:34:57 +0900

    합집합만들고 유니온 파인드 하기

---
+++ graph/1717.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+static vector<int> parent;
+void unionfunc(int a, int b);
+int find(int a);
+bool checkSame(int a, int b);
+
+
+int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+	cout.tie(nullptr);
+    
+    int N,M;
+    cin>>N>>M;
+    parent.resize(N+1);
+
+    for(int i=0;i<=N;i++){
+        parent[i]=i;
+    }
+
+    for(int i=0;i<M;i++){
+        int question,a,b;
+        cin>>question>>a>>b;
+
+        if(question==0){
+            unionfunc(a,b);
+        }
+        else{
+            if(checkSame(a,b)){
+                cout<<"YES"<<"\n";
+            }
+            else{
+                cout<<"NO"<<"\n";
+            }
+        }
+    }
+   
+}
+
+
+void unionfunc(int a,int b){
+    a=find(a);
+    b=find(b);
+
+    if(a!=b){
+        parent[b]=a;
+    }
+}
+
+int find(int a){
+    if(a==parent[a]){
+        return a;
+    }
+    else{
+        return parent[a]=find(parent[a]);
+    }
+}
+
+bool checkSame(int a,int b){
+    a=find(a);
+    b=find(b);
+
+    if(a==b){
+        return true;
+    }
+    return false;
+}
+
+
